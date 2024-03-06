59883a82738cbca6ddb7b39c7b4bc5a8cf761da3
Cecilia <ceciliaperet81@gmail.com>
2024-03-06 19:03:33 -0300

    adicionando classe seller

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,94 @@
+package model.entities;
+import java.io.Serializable;
+import java.util.Date;
+public class Seller implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+    private Department department;
+
+    public Seller() {
+    }
+    
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    // getter e setter 
+
+    public Integer getId() {
+        return id;
+    }   
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    // hashCode e equals
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Seller)) return false;
+        Seller seller = (Seller) o;
+        return getId().equals(seller.getId()) && getName().equals(seller.getName()) && getEmail().equals(seller.getEmail()) && getBirthDate().equals(seller.getBirthDate()) && getBaseSalary().equals(seller.getBaseSalary()) && getDepartment().equals(seller.getDepartment());
+    }
+
+    @Override
+    public int hashCode() { // hashcode de cada um dos atributos
+        return 31 * getId().hashCode() + getName().hashCode() + getEmail().hashCode() +
+         getBirthDate().hashCode() + getBaseSalary().hashCode() + getDepartment().hashCode();
+    }
+
+    
+    
+
+}
